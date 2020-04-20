2d4234c4f40c28751cf554828f33119a949857f4
Kooij <john.van.der.kooij@enexis.nl>
2020-04-20 15:02:22 +0200

    stored procedures

---
+++ StoredProcedures.sql
@@ -0,0 +1,40 @@
+CREATE OR REPLACE PROCEDURE TEST()
+LANGUAGE JAVASCRIPT
+
+AS 
+	  rol = snowflake.execute( { sqlText: `SELECT CURRENT_ROLE();` } );
+      rs = snowflake.execute( { sqlText: "USE ROLE rol;" } ); 
+  
+  
+ 
+ 
+ 
+ 
+ create or replace procedure read_result_set()
+  returns float not null
+  language javascript
+  as     
+  $$  
+    var my_sql_command = "select * from table1";
+    var statement1 = snowflake.createStatement( {sqlText: my_sql_command} );
+    var result_set1 = statement1.execute();
+    // Loop through the results, processing one row at a time... 
+    while (result_set1.next())  {
+       var column1 = result_set1.getColumnValue(1);
+       var column2 = result_set1.getColumnValue(2);
+       // Do something with the retrieved values...
+       }
+  return 0.0; // Replace with something more useful.
+  $$
+  ;
+  
+ 
+ 
+ CREATE OR REPLACE FUNCTION num_test(a double)
+  RETURNS string
+  LANGUAGE JAVASCRIPT
+AS
+$$
+  return A;
+$$
+;
