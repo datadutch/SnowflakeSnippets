2d02940e6b7e3cf4b2e6da7d4130c32020ca7071
Johan <11424878+bidutch@users.noreply.github.com>
2020-12-02 08:50:53 +0100

    Delete Variant.sql

--- Variant.sql
+++
@@ -1,8 +0,0 @@
-USE WAREHOUSE demo_wh;
-USE DATABASE demo_db;
-CREATE TEMP TABLE a (kolom1 variant);
-SELECT * FROM a;
-INSERT INTO a VALUES ('1');
-
---de foutmelding die nu optreedt:
--- Expression type does not match column data type, expecting VARIANT but got VARCHAR(1) for column 
